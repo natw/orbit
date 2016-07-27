4a4c21e392d56cf803a512419fa7f5c659f31c9b
Nathaniel Williams <nat@livewatch.com>
2016-07-27 11:30:25 -0500

    make dot circle around pos

--- src/Main.elm
+++ src/Main.elm
@@ -1,3 +1,4 @@
+import Platform.Cmd as Cmd
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Attributes as Attributes
@@ -7,6 +8,7 @@ import Html.App as App
 import String
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
+import Time
 
 
 main =
@@ -14,56 +16,74 @@ main =
     { init = init
     , view = view
     , update = update
-    , subscriptions = \_ -> Sub.none
+    , subscriptions = subscriptions
   }
 
 
 type alias Model =
   { msg : String
-  , xpos : String
-  , ypos : String
+  , center_xpos : Float
+  , center_ypos : Float
+  , time: Float
   }
 
 
 type Msg =
-  UpdateMessage String | UpdateXPos String | UpdateYPos String
+  Tick Time.Time | UpdateMessage String | UpdateXPos String | UpdateYPos String
 
 
 init : (Model, Cmd Msg)
 init =
-    Model "hey" "0" "0" ! []
+    Model "ok" 0 0 0 ! []
 
 
 view : Model -> Html Msg
 view model =
-  div [ Html.Attributes.class "container" ]
-  [ Svg.svg [ Svg.Attributes.width "200", Svg.Attributes.height "200", viewBox "-100 -100 200 200" ]
-    [ circle [cx model.xpos, cy model.ypos, r "2" ] []
+  let
+    xpos =
+      toString (model.center_xpos + 20 * (cos model.time))
+    ypos =
+      toString (model.center_ypos + 20 * (sin model.time))
+  in
+    div [ Html.Attributes.class "container" ]
+    [ Svg.svg [ Svg.Attributes.width "200", Svg.Attributes.height "200", viewBox "-100 -100 200 200" ]
+      [ circle [cx xpos, cy ypos, r "2" ] []
+      ]
+    , br [] []
+    , label [ for "xpos" ]
+      [ Html.text "xpos"
+      , input [ Attributes.type' "range", Attributes.name "xpos", Attributes.max "100", Attributes.min "-100", onInput UpdateXPos ] [] ]
+    , br [] []
+    , label [ for "ypos" ]
+      [ Html.text "ypos"
+      , input [ Attributes.type' "range", Attributes.name "ypos", Attributes.max "100", Attributes.min "-100", onInput UpdateYPos ] [] ]
+    , br [] []
+    , input [ placeholder "something", onInput UpdateMessage ] []
+    , span [] [ Html.text model.msg ]
     ]
-  , br [] []
-  , label [ for "xpos" ]
-    [ Html.text "xpos"
-    , input [ Attributes.type' "range", Attributes.name "xpos", Attributes.max "100", Attributes.min "-100", onInput UpdateXPos ] [] ]
-  , br [] []
-  , label [ for "ypos" ]
-    [ Html.text "ypos"
-    , input [ Attributes.type' "range", Attributes.name "ypos", Attributes.max "100", Attributes.min "-100", onInput UpdateYPos ] [] ]
-  , br [] []
-  , input [ placeholder "something", onInput UpdateMessage ] []
-  , span [] [ Html.text model.msg ]
-  ]
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
+    Tick time ->
+      { model | time = model.time + 0.01 } ! []
     UpdateMessage str ->
       { model | msg = str } ! []
     UpdateXPos pos ->
-      { model | xpos = pos } ! []
+      case String.toFloat pos of
+        Err msg ->
+          model ! []
+        Ok pos ->
+          { model | center_xpos = pos } ! []
     UpdateYPos pos ->
       case String.toFloat pos of
         Err msg ->
           model ! []
-        Ok ypos ->
-          { model | ypos = toString (-1 * ypos) } ! []
+        Ok pos ->
+          { model | center_ypos = pos } ! []
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Time.every Time.millisecond Tick
